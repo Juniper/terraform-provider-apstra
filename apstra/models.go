4b4b0ab668b533e0b35311d1fa2002564e8b06a1
Chris Marget <cmarget@juniper.net>
2022-07-05 10:48:06 -0400

    sort object declarations

--- apstra/models.go
+++ apstra/models.go
@@ -4,15 +4,46 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-type ResourceManagedDevice struct {
-	AgentId        types.String `tfsdk:"agent_id"`
-	SystemId       types.String `tfsdk:"system_id"`
-	ManagementIp   types.String `tfsdk:"management_ip"`
-	DeviceKey      types.String `tfsdk:"device_key"`
-	AgentProfileId types.String `tfsdk:"agent_profile_id"`
-	AgentLabel     types.String `tfsdk:"agent_label"`
-	Location       types.String `tfsdk:"location"`
-	OnBox          types.Bool   `tfsdk:"on_box"`
+type DataAgentProfile struct {
+	Id          types.String `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Platform    types.String `tfsdk:"platform"`
+	HasUsername types.Bool   `tfsdk:"has_username"`
+	HasPassword types.Bool   `tfsdk:"has_password"`
+	Packages    types.Map    `tfsdk:"packages"`
+	OpenOptions types.Map    `tfsdk:"open_options"`
+}
+
+type DataAgentProfileId struct {
+	Id    types.String   `tfsdk:"id"`
+	Label types.String   `tfsdk:"label"`
+	Tags  []types.String `tfsdk:"tags""`
+}
+
+type DataAgentProfileIds struct {
+	Ids []types.String `tfsdk:"ids"`
+}
+
+type DataAsnPool struct {
+	Id             types.String   `tfsdk:"id"`
+	Name           types.String   `tfsdk:"name"`
+	Status         types.String   `tfsdk:"status"`
+	Tags           []types.String `tfsdk:"tags"`
+	Used           types.Int64    `tfsdk:"used"`
+	UsedPercent    types.Float64  `tfsdk:"used_percentage"`
+	CreatedAt      types.String   `tfsdk:"created_at"`
+	LastModifiedAt types.String   `tfsdk:"last_modified_at"`
+	Total          types.Int64    `tfsdk:"total"`
+	Ranges         []AsnRange     `tfsdk:"ranges"`
+}
+
+type DataAsnPoolId struct {
+	Id   types.String   `tfsdk:"id"`
+	Name types.String   `tfsdk:"name"`
+	Tags []types.String `tfsdk:"tags""`
+}
+type DataAsnPoolIds struct {
+	Ids []types.String `tfsdk:"ids"`
 }
 
 type ResourceAgentProfile struct {
@@ -35,18 +66,18 @@ type ResourceAsnPoolRange struct {
 	Last   types.Int64  `tfsdk:"last"`
 }
 
-type DataAsnPool struct {
-	Id             types.String   `tfsdk:"id"`
-	Name           types.String   `tfsdk:"name"`
-	Status         types.String   `tfsdk:"status"`
-	Tags           []types.String `tfsdk:"tags"`
-	Used           types.Int64    `tfsdk:"used"`
-	UsedPercent    types.Float64  `tfsdk:"used_percentage"`
-	CreatedAt      types.String   `tfsdk:"created_at"`
-	LastModifiedAt types.String   `tfsdk:"last_modified_at"`
-	Total          types.Int64    `tfsdk:"total"`
-	Ranges         []AsnRange     `tfsdk:"ranges"`
+type ResourceManagedDevice struct {
+	AgentId        types.String `tfsdk:"agent_id"`
+	SystemId       types.String `tfsdk:"system_id"`
+	ManagementIp   types.String `tfsdk:"management_ip"`
+	DeviceKey      types.String `tfsdk:"device_key"`
+	AgentProfileId types.String `tfsdk:"agent_profile_id"`
+	AgentLabel     types.String `tfsdk:"agent_label"`
+	Location       types.String `tfsdk:"location"`
+	OnBox          types.Bool   `tfsdk:"on_box"`
 }
+
+// helper structs used by 'resource' and 'data source' objects follow
 
 type AsnRange struct {
 	Status         types.String  `tfsdk:"status"`
@@ -55,34 +86,4 @@ type AsnRange struct {
 	Total          types.Int64   `tfsdk:"total"`
 	Used           types.Int64   `tfsdk:"used"`
 	UsedPercentage types.Float64 `tfsdk:"used_percentage"`
-}
-
-type DataAsnPoolIds struct {
-	Ids []types.String `tfsdk:"ids"`
-}
-
-type DataAsnPoolId struct {
-	Id   types.String   `tfsdk:"id"`
-	Name types.String   `tfsdk:"name"`
-	Tags []types.String `tfsdk:"tags""`
-}
-
-type DataAgentProfileIds struct {
-	Ids []types.String `tfsdk:"ids"`
-}
-
-type DataAgentProfileId struct {
-	Id    types.String   `tfsdk:"id"`
-	Label types.String   `tfsdk:"label"`
-	Tags  []types.String `tfsdk:"tags""`
-}
-
-type DataAgentProfile struct {
-	Id          types.String `tfsdk:"id"`
-	Name        types.String `tfsdk:"name"`
-	Platform    types.String `tfsdk:"platform"`
-	HasUsername types.Bool   `tfsdk:"has_username"`
-	HasPassword types.Bool   `tfsdk:"has_password"`
-	Packages    types.Map    `tfsdk:"packages"`
-	OpenOptions types.Map    `tfsdk:"open_options"`
 }
