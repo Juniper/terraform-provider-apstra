c70feeb28e04775db6517a4e9523f4fbe531a2d4
Chris Marget <cmarget@juniper.net>
2022-09-08 17:56:58 -0400

    add rack type example

--- examples/resources/apstra_rack_type/example.tf
+++ examples/resources/apstra_rack_type/example.tf
@@ -1,1 +1,13 @@
-# todo
+resource "apstra_rack_type" "rt" {
+  name = "terraform"
+  description = "terraform did this"
+  fabric_connectivity_design = "l3clos"
+  leaf_switches = [
+    {
+      name = "leafy"
+      logical_device_id = "AOS-7x10-Leaf"
+      spine_link_count = 1
+      spine_link_speed = "10G"
+    }
+  ]
+}
