114a7223691debffc50123819e0eb6cf20c8e636
Chris Marget <cmarget@juniper.net>
2022-11-10 21:32:24 -0500

    add test files

---
+++ test/resource_logical_device.tf
@@ -0,0 +1,16 @@
+#resource "apstra_logical_device" "ld" {
+#  name = "foo"
+#  panels = [
+#    {
+#      rows = 1
+#      columns = 1
+#      port_groups = [
+#        {
+#          port_count = 1
+#          port_speed = "10G"
+#          port_roles = ["spine", "leaf"]
+#        }
+#      ]
+#    }
+#  ]
+#}
