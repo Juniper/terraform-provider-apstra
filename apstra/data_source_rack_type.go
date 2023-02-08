2b5040dde8cdc5516571af0eb58e8c0382696fa8
Chris Marget <cmarget@juniper.net>
2023-02-08 18:13:56 -0500

    another use of stringValueOrNull()

--- apstra/data_source_rack_type.go
+++ apstra/data_source_rack_type.go
@@ -212,16 +212,9 @@ func (o *dRackType) loadApiResponse(ctx context.Context, in *goapstra.RackType, 
 		}
 	}
 
-	var description types.String
-	if in.Data.Description == "" {
-		description = types.StringNull()
-	} else {
-		description = types.StringValue(in.Data.Description)
-	}
-
 	o.Id = types.StringValue(string(in.Id))
 	o.Name = types.StringValue(in.Data.DisplayName)
-	o.Description = description
+	o.Description = stringValueOrNull(ctx, string(in.Data.Description), diags)
 	o.FabricConnectivityDesign = types.StringValue(in.Data.FabricConnectivityDesign.String())
 	o.LeafSwitches = mapValueOrNull(ctx, dRackTypeLeafSwitch{}.attrType(), leafSwitches, diags)
 	o.AccessSwitches = mapValueOrNull(ctx, dRackTypeAccessSwitch{}.attrType(), accessSwitches, diags)
