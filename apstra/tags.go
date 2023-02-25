57e9e123d76b5b8060195b707a6040d22881cb07
Chris Marget <cmarget@juniper.net>
2023-02-25 08:06:52 -0500

    clean up unused tag code

--- apstra/tags.go
+++ apstra/tags.go
@@ -4,58 +4,14 @@ import (
 	"bitbucket.org/apstrktr/goapstra"
 	"context"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
-	dataSourceSchema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-//func tagIdsAttributeSchema() tfsdk.Attribute {
-//	return tfsdk.Attribute{
-//		MarkdownDescription: "IDs of tags from the global catalog to be applied to this element upon creation.",
-//		Optional:            true,
-//		Type:                types.SetType{ElemType: types.StringType},
-//		Validators:          []tfsdk.AttributeValidator{setvalidator.SizeAtLeast(1)},
-//		//PlanModifiers:       []tfsdk.AttributePlanModifier{useStateForUnknownNull()},
-//		//PlanModifiers: []tfsdk.AttributePlanModifier{resource.UseStateForUnknown()},
-//	}
-//}
-
-//func tagsDataAttributeSchema() schema.SetNestedAttribute {
-//	return schema.SetNestedAttribute{
-//		MarkdownDescription: "Details any tags applied to the element.",
-//		Computed:            true,
-//		NestedObject: schema.NestedAttributeObject{
-//			Attributes: map[string]schema.Attribute{
-//				"name": schema.StringAttribute{
-//					MarkdownDescription: "Tag name (label) field.",
-//					Computed:            true,
-//				},
-//				"description": schema.StringAttribute{
-//					MarkdownDescription: "Tag description field.",
-//					Computed:            true,
-//				},
-//			},
-//		},
-//	}
-//}
-
 type tagData struct {
 	Name        string `tfsdk:"name"`
 	Description string `tfsdk:"description"`
-}
-
-func (o tagData) dataSourceAttributes() map[string]dataSourceSchema.Attribute {
-	return map[string]dataSourceSchema.Attribute{
-		"name": dataSourceSchema.StringAttribute{
-			MarkdownDescription: "Tag name (label) field.",
-			Computed:            true,
-		},
-		"description": dataSourceSchema.StringAttribute{
-			MarkdownDescription: "Tag description field.",
-			Computed:            true,
-		},
-	}
 }
 
 func (o tagData) resourceAttributes() map[string]resourceSchema.Attribute {
@@ -76,11 +32,6 @@ func (o tagData) attrTypes() map[string]attr.Type {
 		"name":        types.StringType,
 		"description": types.StringType,
 	}
-}
-
-func (o *tagData) parseApi(in *goapstra.DesignTagData) {
-	o.Name = in.Label
-	o.Description = in.Description
 }
 
 func newTagDataSet(ctx context.Context, in []goapstra.DesignTagData, diags *diag.Diagnostics) types.Set {
