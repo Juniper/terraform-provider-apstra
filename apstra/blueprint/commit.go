7b941a274072d84ce064a1728dbb1461da039415
Chris Marget <cmarget@juniper.net>
2023-03-08 12:48:30 -0500

    add comment field to commit schema

--- apstra/blueprint/commit.go
+++ apstra/blueprint/commit.go
@@ -9,13 +9,19 @@ import (
 
 type Commit struct {
 	BlueprintId types.String `tfsdk:"blueprint_id"`
+	Comment     types.String `tfsdk:"comment"`
 }
 
 func (o Commit) ResourceAttributes() map[string]resourceSchema.Attribute {
 	return map[string]resourceSchema.Attribute{
 		"blueprint_id": resourceSchema.StringAttribute{
-			MarkdownDescription: "",
+			MarkdownDescription: "", // todo
 			Required:            true,
+			Validators:          []validator.String{stringvalidator.LengthAtLeast(1)},
+		},
+		"comment": resourceSchema.StringAttribute{
+			MarkdownDescription: "", // todo
+			Optional:            true,
 			Validators:          []validator.String{stringvalidator.LengthAtLeast(1)},
 		},
 	}
