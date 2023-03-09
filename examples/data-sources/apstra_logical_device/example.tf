048eb8ad694ff4973be996fa978b8b51425818fe
Chris Marget <cmarget@juniper.net>
2023-03-09 17:16:15 -0500

    more examples

--- examples/data-sources/apstra_logical_device/example.tf
+++ examples/data-sources/apstra_logical_device/example.tf
@@ -1,29 +1,36 @@
-# The following example shows how a module might accept a logical device name
-# as an input variable and use it to retrieve the agent profile ID when
-# provisioning a rack type.
+# The following example shows how a module might accept a Logical Device's name
+# as an input variable and then use it to retrieve the Logical Device ID when
+# provisioning a Rack Type.
 
+# module input variable has the Logical Device name
 variable "logical_device_name" {
     type = string
-    default = "AOS-7x10-Leaf"
+    default = "AOS-24x10-2"
 }
 
+# Data lookup using the Logical Device name. Name collisions are possible,
+# will produce an error. Apstra permits name collisions for many object types.
+# It's probably best to avoid creating them.
 data "apstra_logical_device" "selected" {
     name = var.logical_device_name
 }
 
+# Create a Rack Type using the Logical Device ID found in the object which
+# was looked up by name.
 resource "apstra_rack_type" "my_rack" {
     name = "terraform, yo"
     fabric_connectivity_design = "l3clos"
     leaf_switches = {
         leaf_one = {
-            spine_link_count = 1
+            spine_link_count = 2
             spine_link_speed = "10G"
             logical_device_id = data.apstra_logical_device.selected.id
+            redundancy_protocol = "esi"
         }
     }
     access_switches = {
         access_one = {
-            count = 1
+            count = 3
             logical_device_id = data.apstra_logical_device.selected.id
             links = {
                 link_one = {
@@ -31,6 +38,10 @@ resource "apstra_rack_type" "my_rack" {
                     links_per_switch = 2
                     target_switch_name = "leaf_one"
                 }
+            }
+            esi_lag_info = {
+                l3_peer_link_count = 2
+                l3_peer_link_speed = "10G"
             }
         }
     }
