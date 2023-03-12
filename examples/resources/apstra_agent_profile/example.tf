1c730c376d7f9a2deed5b562ca9902186ace8b7c
Chris Marget <cmarget@juniper.net>
2023-03-12 17:25:22 -0400

    ../examples/resources/apstra_agent_profile/example.tf

--- examples/resources/apstra_agent_profile/example.tf
+++ examples/resources/apstra_agent_profile/example.tf
@@ -1,1 +1,13 @@
-# todo
+# This example creates an Agent Profile with a couple of options.
+# Note that it is not possible to set device credentials with this
+# resource because (a) the credentials cannot be retrieved, so
+# terraform cannot manage the full lifecycle and (b) keeping
+# secrets in the state file probably isn't great.
+resource "apstra_agent_profile" "profile_with_options" {
+  name = "spine switches"
+  platform = "eos"
+  open_options = {
+    foo = "bar"
+    baz = "not bar"
+  }
+}
