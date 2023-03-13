bdca45f6b3192849ca37d3815da6d1b93cc45b18
Chris Marget <cmarget@juniper.net>
2023-03-12 23:12:56 -0400

    ../examples/resources/apstra_tag/example.tf

--- examples/resources/apstra_tag/example.tf
+++ examples/resources/apstra_tag/example.tf
@@ -1,1 +1,16 @@
-# todo
+# this example creates a tags named after enterprise teams
+# responsible for various data center asset types.
+locals {
+  device_owners = toset([
+    "research",
+    "security",
+    "app team 1",
+    "app team 2",
+  ])
+}
+
+resource "apstra_tag" "example" {
+  for_each    = local.device_owners
+  name        = each.key
+  description = format("device maintained by %q team", each.key)
+}
