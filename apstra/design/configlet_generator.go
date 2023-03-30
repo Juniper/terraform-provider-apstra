d524ec6d168922777dcde2a86c1bc8c698bf3bf1
Rajagopalan Subrahmanian <rsubrahmania@juniper.net>
2023-03-30 13:57:47 -0400

    move generator to own file

--- apstra/design/configlet_generator.go
+++ apstra/design/configlet_generator.go
@@ -11,7 +11,6 @@ import (
 	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"strings"
 	"terraform-provider-apstra/apstra/utils"
 )
@@ -119,49 +118,4 @@ func (o *ConfigletGenerator) Request(_ context.Context, diags *diag.Diagnostics)
 		NegationTemplateText: o.NegationTemplateText.ValueString(),
 		Filename:             o.FileName.ValueString(),
 	}
-}
-
-var _ validator.Object = ConfigletGeneratorValidator{}
-
-type ConfigletGeneratorValidator struct {
-}
-
-func (o ConfigletGeneratorValidator) Description(_ context.Context) string {
-	return fmt.Sprintf("Ensures that the section name matches the config style.")
-}
-
-func (o ConfigletGeneratorValidator) MarkdownDescription(ctx context.Context) string {
-	return o.Description(ctx)
-}
-
-func (o ConfigletGeneratorValidator) ValidateObject(ctx context.Context, req validator.ObjectRequest, resp *validator.ObjectResponse) {
-	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
-		return
-	}
-	var c ConfigletGenerator
-	//resp.Diagnostics.Append(req.Config.Get(ctx, &c)...)
-	resp.Diagnostics.Append(req.ConfigValue.As(ctx, &c, basetypes.ObjectAsOptions{})...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	cg := c.Request(ctx, &resp.Diagnostics)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	valid := false
-	for _, i := range cg.ConfigStyle.ValidSections() {
-		if i == cg.Section {
-			valid = true
-			goto done
-		}
-	}
-done:
-	if !valid {
-		resp.Diagnostics.AddError("Invalid Section", fmt.Sprintf("Invalid Section %q used for Config Style %q", cg.Section.String(), cg.ConfigStyle.String()))
-	}
-	return
-}
-
-func ValidateConfigletGenerator() validator.Object {
-	return ConfigletGeneratorValidator{}
 }
---
+++ apstra/design/configlet_generator_validator.go
@@ -0,0 +1,53 @@
+package design
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+var _ validator.Object = ConfigletGeneratorValidator{}
+
+type ConfigletGeneratorValidator struct {
+}
+
+func (o ConfigletGeneratorValidator) Description(_ context.Context) string {
+	return fmt.Sprintf("Ensures that the section name matches the config style.")
+}
+
+func (o ConfigletGeneratorValidator) MarkdownDescription(ctx context.Context) string {
+	return o.Description(ctx)
+}
+
+func (o ConfigletGeneratorValidator) ValidateObject(ctx context.Context, req validator.ObjectRequest, resp *validator.ObjectResponse) {
+	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+		return
+	}
+	var c ConfigletGenerator
+	//resp.Diagnostics.Append(req.Config.Get(ctx, &c)...)
+	resp.Diagnostics.Append(req.ConfigValue.As(ctx, &c, basetypes.ObjectAsOptions{})...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	cg := c.Request(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	valid := false
+	for _, i := range cg.ConfigStyle.ValidSections() {
+		if i == cg.Section {
+			valid = true
+			goto done
+		}
+	}
+done:
+	if !valid {
+		resp.Diagnostics.AddError("Invalid Section", fmt.Sprintf("Invalid Section %q used for Config Style %q", cg.Section.String(), cg.ConfigStyle.String()))
+	}
+	return
+}
+
+func ValidateConfigletGenerator() validator.Object {
+	return ConfigletGeneratorValidator{}
+}
