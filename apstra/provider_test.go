bc087c1c975f4f011f9ef4dd111c4cf41b9573f7
Chris Marget <cmarget@juniper.net>
2023-04-18 18:06:22 -0400

    first acceptance testing: resources

---
+++ apstra/provider_test.go
@@ -0,0 +1,27 @@
+package tfapstra
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+)
+
+const (
+	providerConfigHCL = `
+provider "apstra" {
+  blueprint_mutex_disabled = true
+}
+`
+
+	insecureProviderConfigHCL = `
+provider "apstra" {
+  tls_validation_disabled = true
+  blueprint_mutex_disabled = true
+}
+`
+)
+
+var (
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"apstra": providerserver.NewProtocol6WithError(NewProvider()),
+	}
+)
