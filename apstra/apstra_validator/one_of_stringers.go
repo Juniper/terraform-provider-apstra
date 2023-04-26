be3f234e57db1b7b7c91803327c9fa4b0753b913
Chris Marget <cmarget@juniper.net>
2023-04-25 20:50:58 -0400

    use generics for OneOfStringers()

--- apstra/apstra_validator/one_of_stringers.go
+++ apstra/apstra_validator/one_of_stringers.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
-func OneOfStringers(s ...fmt.Stringer) validator.String {
+func OneOfStringers[S fmt.Stringer](s []S) validator.String {
 	strings := make([]string, len(s))
 	for i := range s {
 		strings[i] = s[i].String()
