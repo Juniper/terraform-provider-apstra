230c1cb75dc9183ab0d576aed8dfc2cb96a1ca4f
Chris Marget <cmarget@juniper.net>
2023-04-27 18:41:28 -0400

    resource virtual_network WIP

--- apstra/resource_datacenter_virtual_network.go
+++ apstra/resource_datacenter_virtual_network.go
@@ -1,64 +1,200 @@
 package tfapstra
 
-//
-//import (
-//	"context"
-//	"fmt"
-//	"github.com/Juniper/apstra-go-sdk/apstra"
-//	"github.com/hashicorp/terraform-plugin-framework/resource"
-//	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-//	"terraform-provider-apstra/apstra/blueprint"
-//)
-//
-//var _ resource.ResourceWithConfigure = &resourceDatacenterVirtualNetwork{}
-//
-//type resourceDatacenterVirtualNetwork struct {
-//	client   *apstra.Client
-//	lockFunc func(context.Context, string) error
-//}
-//
-//func (o *resourceDatacenterVirtualNetwork) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-//	resp.TypeName = req.ProviderTypeName + "_datacenter_virtual_network"
-//}
-//
-//func (o *resourceDatacenterVirtualNetwork) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
-//	o.client = ResourceGetClient(ctx, req, resp)
-//	o.lockFunc = ResourceGetBlueprintLockFunc(ctx, req, resp)
-//}
-//
-//func (o *resourceDatacenterVirtualNetwork) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
-//	resp.Schema = schema.Schema{
-//		MarkdownDescription: "This resource creates a Virtual Network within a Blueprint.",
-//		Attributes:          blueprint.DatacenterVirtualNetwork{}.ResourceAttributes(),
-//	}
-//}
-//
-//func (o *resourceDatacenterVirtualNetwork) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-//	if o.client == nil {
-//		resp.Diagnostics.AddError(errResourceUnconfiguredSummary, errResourceUnconfiguredCreateDetail)
-//		return
-//	}
-//
-//	// Retrieve values from plan.
-//	var plan blueprint.DatacenterVirtualNetwork
-//	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-//	if resp.Diagnostics.HasError() {
-//		return
-//	}
-//
-//	// Lock the blueprint mutex.
-//	err := o.lockFunc(ctx, plan.BlueprintId.ValueString())
-//	if err != nil {
-//		resp.Diagnostics.AddError(
-//			fmt.Sprintf("error locking blueprint %q mutex", plan.BlueprintId.ValueString()),
-//			err.Error())
-//		return
-//	}
-//
-//	bp, err := o.client.NewTwoStageL3ClosClient(ctx, apstra.ObjectId(plan.BlueprintId.ValueString()))
-//	if err != nil {
-//		resp.Diagnostics.AddError("error creating blueprint client", err.Error())
-//		return
-//	}
-//
-//}
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"terraform-provider-apstra/apstra/blueprint"
+)
+
+var _ resource.ResourceWithConfigure = &resourceDatacenterVirtualNetwork{}
+
+type resourceDatacenterVirtualNetwork struct {
+	client   *apstra.Client
+	lockFunc func(context.Context, string) error
+}
+
+func (o *resourceDatacenterVirtualNetwork) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_datacenter_virtual_network"
+}
+
+func (o *resourceDatacenterVirtualNetwork) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	o.client = ResourceGetClient(ctx, req, resp)
+	o.lockFunc = ResourceGetBlueprintLockFunc(ctx, req, resp)
+}
+
+func (o *resourceDatacenterVirtualNetwork) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "This resource creates a Virtual Network within a Blueprint.",
+		Attributes:          blueprint.DatacenterVirtualNetwork{}.ResourceAttributes(),
+	}
+}
+
+func (o *resourceDatacenterVirtualNetwork) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	if o.client == nil {
+		resp.Diagnostics.AddError(errResourceUnconfiguredSummary, errResourceUnconfiguredCreateDetail)
+		return
+	}
+
+	// Retrieve values from plan.
+	var plan blueprint.DatacenterVirtualNetwork
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Lock the blueprint mutex.
+	err := o.lockFunc(ctx, plan.BlueprintId.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			fmt.Sprintf("error locking blueprint %q mutex", plan.BlueprintId.ValueString()),
+			err.Error())
+		return
+	}
+
+	// create a request object
+	request := plan.Request(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// create a client for the datacenter reference design
+	bp, err := o.client.NewTwoStageL3ClosClient(ctx, apstra.ObjectId(plan.BlueprintId.ValueString()))
+	if err != nil {
+		resp.Diagnostics.AddError("error creating blueprint client", err.Error())
+		return
+	}
+
+	// create the virtual network
+	id, err := bp.CreateVirtualNetwork(ctx, request)
+	if err != nil {
+		resp.Diagnostics.AddError("error creating virtual network", err.Error())
+	}
+
+	// update the plan with the received ObjectId and set the state
+	plan.Id = types.StringValue(id.String())
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+}
+
+func (o *resourceDatacenterVirtualNetwork) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	if o.client == nil {
+		resp.Diagnostics.AddError(errResourceUnconfiguredSummary, errResourceUnconfiguredReadDetail)
+		return
+	}
+
+	// Retrieve values from state.
+	var state blueprint.DatacenterVirtualNetwork
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// create a client for the datacenter reference design
+	bp, err := o.client.NewTwoStageL3ClosClient(ctx, apstra.ObjectId(state.BlueprintId.ValueString()))
+	if err != nil {
+		var ace apstra.ApstraClientErr
+		if errors.As(err, &ace) && ace.Type() == apstra.ErrNotfound {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+		resp.Diagnostics.AddError("error creating blueprint client", err.Error())
+		return
+	}
+
+	// retrieve the virtual network
+	vn, err := bp.GetVirtualNetwork(ctx, apstra.ObjectId(state.Id.ValueString()))
+	if err != nil {
+		var ace apstra.ApstraClientErr
+		if errors.As(err, &ace) && ace.Type() == apstra.ErrNotfound {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+	}
+
+	// load the API response and set the state
+	state.LoadApiData(ctx, vn.Data, &resp.Diagnostics)
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
+}
+
+func (o *resourceDatacenterVirtualNetwork) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	if o.client == nil {
+		resp.Diagnostics.AddError(errResourceUnconfiguredSummary, errResourceUnconfiguredUpdateDetail)
+		return
+	}
+
+	// Retrieve values from plan.
+	var plan blueprint.DatacenterVirtualNetwork
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Lock the blueprint mutex.
+	err := o.lockFunc(ctx, plan.BlueprintId.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			fmt.Sprintf("error locking blueprint %q mutex", plan.BlueprintId.ValueString()),
+			err.Error())
+		return
+	}
+
+	// create a request object
+	request := plan.Request(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// create a client for the datacenter reference design
+	bp, err := o.client.NewTwoStageL3ClosClient(ctx, apstra.ObjectId(plan.BlueprintId.ValueString()))
+	if err != nil {
+		resp.Diagnostics.AddError("error creating blueprint client", err.Error())
+		return
+	}
+
+	// update the virtual network according to the plan
+	err = bp.UpdateVirtualNetwork(ctx, apstra.ObjectId(plan.Id.ValueString()), request)
+	if err != nil {
+		resp.Diagnostics.AddError("error updating virtual network", err.Error())
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+}
+
+func (o *resourceDatacenterVirtualNetwork) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	if o.client == nil {
+		resp.Diagnostics.AddError(errResourceUnconfiguredSummary, errResourceUnconfiguredDeleteDetail)
+		return
+	}
+
+	// Retrieve values from state.
+	var state blueprint.DatacenterVirtualNetwork
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// create a client for the datacenter reference design
+	bp, err := o.client.NewTwoStageL3ClosClient(ctx, apstra.ObjectId(state.BlueprintId.ValueString()))
+	if err != nil {
+		var ace apstra.ApstraClientErr
+		if errors.As(err, &ace) && ace.Type() == apstra.ErrNotfound {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+		resp.Diagnostics.AddError("error creating blueprint client", err.Error())
+		return
+	}
+
+	err = bp.DeleteVirtualNetwork(ctx, apstra.ObjectId(state.Id.ValueString()))
+	if err != nil {
+		var ace apstra.ApstraClientErr
+		if errors.As(err, &ace) && ace.Type() == apstra.ErrNotfound {
+			return
+		}
+		resp.Diagnostics.AddError("error deleting virtual network", err.Error())
+	}
+}
