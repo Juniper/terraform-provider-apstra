82c0cd99279560bd9dd0e5c751d3e56dff274c77
Rajagopalan Subrahmanian <rsubrahmania@juniper.net>
2023-05-04 02:00:24 -0400

    clean up provider.tf

--- test/provider.tf
+++ test/provider.tf
@@ -1,14 +1,12 @@
 terraform {
   required_providers {
     apstra = {
-      source = "example.com/juniper/apstra"
-    }
+      source = "juniper/apstra"
+    
   }
 }
 
 provider "apstra" {
   tls_validation_disabled = true
   blueprint_mutex_disabled = true
- # url = "https://admin:HappyDuck6%5E@35.92.136.236:32909"
-  url = "https://admin:admin@10.6.1.44/"
 }
