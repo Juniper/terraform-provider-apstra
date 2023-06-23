25890abb8333e135cf7a98374b04cceedb5ffb4d
Rajagopalan Subrahmanian <rsubrahmania@juniper.net>
2023-06-23 17:25:09 -0400

    fix tests and documentation

--- examples/resources/apstra_datacenter_property_set/example.tf
+++ examples/resources/apstra_datacenter_property_set/example.tf
@@ -1,6 +1,6 @@
 #reference a blueprint
 data "apstra_datacenter_blueprint" "b" {
-	name = "raj1"
+	name = "demo1"
 }
 #reference a property set from the global catalog
 data "apstra_property_set" "ps" {
@@ -10,8 +10,7 @@ data "apstra_property_set" "ps" {
 resource "apstra_datacenter_property_set" "r" {
 	blueprint_id = data.apstra_datacenter_blueprint.b.id
 	id = data.apstra_property_set.ps.id
-# uncomment below to import only one key. If no keys are mentioned, all keys are imported
-#	keys = toset(["junos_mgmt_vrf"])
+	keys = data.apstra_property_set.ps.keys
 }
 output "p" {
 	value = resource.apstra_datacenter_property_set.r
@@ -21,7 +20,10 @@ output "p" {
 #	"blueprint_id" = "d6c74373-45ce-4d88-9547-ac23c2ebe61e"
 #	"data" = "{\"junos_mgmt_vrf\": \"mgmt_junos\", \"mgmt_vrf\": \"management\"}"
 #	"id" = "3ae45f2e-c9ed-401b-8f00-367fb9a5e0e8"
-#	"keys" = toset(null) /* of string */
+#	"keys" =  toset([
+#   "junos_mgmt_vrf",
+#   "mgmt_vrf",
+#   ])
 #	"name" = "MGMT VRF"
 #	"stale" = false
 #}
