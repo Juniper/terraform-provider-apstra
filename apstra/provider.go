c63ebfda26d16c48b0d6542157a7803da69a9bc4
Chris Marget <cmarget@juniper.net>
2023-08-22 21:05:22 -0400

    introduce `apstra_datacenter_interface_ids_by_link_tag`

--- apstra/provider.go
+++ apstra/provider.go
@@ -365,6 +365,7 @@ func (p *Provider) DataSources(_ context.Context) []func() datasource.DataSource
 		func() datasource.DataSource { return &dataSourceDatacenterSystemNodes{} },
 		func() datasource.DataSource { return &dataSourceDatacenterVirtualNetworks{} },
 		func() datasource.DataSource { return &dataSourceIntegerPool{} },
+		func() datasource.DataSource { return &dataSourceInterfacesByLinkTag{} },
 		func() datasource.DataSource { return &dataSourceIntegerPools{} },
 		func() datasource.DataSource { return &dataSourceInterfaceMap{} },
 		func() datasource.DataSource { return &dataSourceInterfaceMaps{} },
