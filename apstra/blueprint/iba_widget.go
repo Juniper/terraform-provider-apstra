661b25d8d2cd8f60b46bba3905065afd8e49c95a
Rajagopalan Subrahmanian <rsubrahmania@juniper.net>
2023-10-03 09:44:41 -0400

    Add support for iba widget data source

---
+++ apstra/blueprint/iba_widget.go
@@ -0,0 +1,59 @@
+package blueprint
+
+import (
+	"context"
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	dataSourceSchema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type IbaWidget struct {
+	BlueprintId types.String `tfsdk:"blueprint_id"`
+	Id          types.String `tfsdk:"id"`
+	Label       types.String `tfsdk:"label"`
+	Description types.String `tfsdk:"description"`
+}
+
+func (o IbaWidget) DataSourceAttributes() map[string]dataSourceSchema.Attribute {
+	return map[string]dataSourceSchema.Attribute{
+		"blueprint_id": dataSourceSchema.StringAttribute{
+			MarkdownDescription: "Apstra Blueprint ID. Used to identify the Blueprint that the IBA Widget belongs to.",
+			Required:            true,
+			Validators:          []validator.String{stringvalidator.LengthAtLeast(1)},
+		},
+		"id": dataSourceSchema.StringAttribute{
+			MarkdownDescription: "Populate this field to look up a IBA Widget by ID. Required when `name` is omitted.",
+			Optional:            true,
+			Computed:            true,
+			Validators: []validator.String{
+				stringvalidator.LengthAtLeast(1),
+				stringvalidator.ExactlyOneOf(path.Expressions{
+					path.MatchRelative(),
+					path.MatchRoot("label"),
+				}...),
+			},
+		},
+		"label": dataSourceSchema.StringAttribute{
+			MarkdownDescription: "Populate this field to look up a IBA Widget by name. Required when `id` is omitted.",
+			Optional:            true,
+			Computed:            true,
+			Validators: []validator.String{
+				stringvalidator.LengthAtLeast(1),
+			},
+		},
+		"description": dataSourceSchema.StringAttribute{
+			MarkdownDescription: "Description of the IBA Widget",
+			Computed:            true,
+		},
+	}
+}
+
+func (o *IbaWidget) LoadApiData(ctx context.Context, in *apstra.IbaWidget, _ *diag.Diagnostics) {
+	o.Id = types.StringValue(in.Id.String())
+	o.Label = types.StringValue(in.Data.Label)
+	o.Description = types.StringValue(in.Data.Description)
+}
---
+++ apstra/data_source_datacenter_iba_widget.go
@@ -0,0 +1,96 @@
+package tfapstra
+
+import (
+	"context"
+	"fmt"
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	"github.com/Juniper/terraform-provider-apstra/apstra/blueprint"
+	"github.com/Juniper/terraform-provider-apstra/apstra/utils"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+)
+
+var _ datasource.DataSourceWithConfigure = &dataSourceDatacenterIbaWidget{}
+
+type dataSourceDatacenterIbaWidget struct {
+	client *apstra.Client
+}
+
+func (o *dataSourceDatacenterIbaWidget) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_datacenter_iba_widget"
+}
+
+func (o *dataSourceDatacenterIbaWidget) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	o.client = DataSourceGetClient(ctx, req, resp)
+}
+
+func (o *dataSourceDatacenterIbaWidget) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "This data source provides details of a specific IBA Widget in a Blueprint." +
+			"\n\n" +
+			"At least one optional attribute is required.",
+		Attributes: blueprint.IbaWidget{}.DataSourceAttributes(),
+	}
+}
+
+func (o *dataSourceDatacenterIbaWidget) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var config blueprint.IbaWidget
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	bpClient, err := o.client.NewTwoStageL3ClosClient(ctx, apstra.ObjectId(config.BlueprintId.ValueString()))
+	if err != nil {
+		if utils.IsApstra404(err) {
+			resp.Diagnostics.AddError(fmt.Sprintf("blueprint %s not found",
+				config.BlueprintId), err.Error())
+			return
+		}
+		resp.Diagnostics.AddError("failed to create blueprint client", err.Error())
+		return
+	}
+
+	var api *apstra.IbaWidget
+	switch {
+	case !config.Label.IsNull():
+		api, err = bpClient.GetIbaWidgetByLabel(ctx, config.Label.ValueString())
+		if err != nil {
+			if utils.IsApstra404(err) {
+				resp.Diagnostics.AddAttributeError(
+					path.Root("name"),
+					"IBA widget not found",
+					fmt.Sprintf("IBA Widget with label %s not found", config.Label))
+				return
+			}
+			resp.Diagnostics.AddAttributeError(
+				path.Root("name"), "Failed reading IBA Widget", err.Error(),
+			)
+			return
+		}
+	case !config.Id.IsNull():
+		api, err = bpClient.GetIbaWidget(ctx, apstra.ObjectId(config.Id.ValueString()))
+		if err != nil {
+			if utils.IsApstra404(err) {
+				resp.Diagnostics.AddAttributeError(
+					path.Root("id"),
+					"IbaWidget not found",
+					fmt.Sprintf("IbaWidget with ID %s not found", config.Id))
+				return
+			}
+			resp.Diagnostics.AddAttributeError(
+				path.Root("name"), "Failed reading IBA Widgett", err.Error(),
+			)
+			return
+		}
+	}
+
+	config.LoadApiData(ctx, api, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Set state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &config)...)
+}
