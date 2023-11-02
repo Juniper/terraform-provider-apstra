Juniper/terraform-provider-apstra

commit 423e0a165a305625e7dcfe8f674ca023ceb59832

make unused function arguments unnamed

apstra/iba/widget.go
@@ -118,7 +118,7 @@ func (o *Widget) LoadApiData(ctx context.Context, in *apstra.IbaWidget, d *diag.
 	o.ProbeId = types.StringValue(in.Data.ProbeId.String())
 }
 
-func (o *Widget) Request(ctx context.Context, d *diag.Diagnostics) *apstra.IbaWidgetData {
+func (o *Widget) Request(_ context.Context, _ diag.Diagnostics) *apstra.IbaWidgetData {
 	return &apstra.IbaWidgetData{
 		StageName:   o.Stage.ValueString(),
 		Description: o.Description.ValueString(),

commit 0c5375581a1f61bd38b6b7fad80a0cb4cc8ddf8a

fix pointer broken in 423e0a165a305625e7dcfe8f674ca023ceb59832

apstra/iba/widget.go
@@ -118,7 +118,7 @@ func (o *Widget) LoadApiData(ctx context.Context, in *apstra.IbaWidget, d *diag.
 	o.ProbeId = types.StringValue(in.Data.ProbeId.String())
 }
 
-func (o *Widget) Request(_ context.Context, _ diag.Diagnostics) *apstra.IbaWidgetData {
+func (o *Widget) Request(_ context.Context, _ *diag.Diagnostics) *apstra.IbaWidgetData {
 	return &apstra.IbaWidgetData{
 		StageName:   o.Stage.ValueString(),
 		Description: o.Description.ValueString(),