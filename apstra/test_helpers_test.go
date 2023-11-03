4f90b6da9763971a4a1d764577ec4284ba400cee
Chris Marget <cmarget@juniper.net>
2023-11-03 19:58:41 -0400

    comment out future-use functions

--- apstra/test_helpers_test.go
+++ apstra/test_helpers_test.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/Juniper/apstra-go-sdk/apstra"
-	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"golang.org/x/exp/constraints"
-	"net"
 	"testing"
 )
 
@@ -42,19 +40,19 @@ func systemIds(ctx context.Context, t *testing.T, bp *apstra.TwoStageL3ClosClien
 	return ids
 }
 
-func stringPtrOrNull(in *string) string {
-	if in == nil {
-		return "null"
-	}
-	return `"` + *in + `"`
-}
+//func stringPtrOrNull(in *string) string {
+//	if in == nil {
+//		return "null"
+//	}
+//	return `"` + *in + `"`
+//}
 
-func stringOrNull(in string) string {
-	if in == "" {
-		return "null"
-	}
-	return `"` + in + `"`
-}
+//func stringOrNull(in string) string {
+//	if in == "" {
+//		return "null"
+//	}
+//	return `"` + in + `"`
+//}
 
 func intPtrOrNull[A constraints.Integer](in *A) string {
 	if in == nil {
@@ -63,23 +61,23 @@ func intPtrOrNull[A constraints.Integer](in *A) string {
 	return fmt.Sprintf("%d", *in)
 }
 
-func ipOrNull(in *net.IPNet) string {
-	if in == nil {
-		return "null"
-	}
-	return `"` + in.String() + `"`
-}
+//func ipOrNull(in *net.IPNet) string {
+//	if in == nil {
+//		return "null"
+//	}
+//	return `"` + in.String() + `"`
+//}
 
-func randIpAddressMust(t *testing.T, cidrBlock string) net.IP {
-	s, err := acctest.RandIpAddress(cidrBlock)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ip := net.ParseIP(s)
-	if ip == nil {
-		t.Fatalf("randIpAddressMust failed to parse IP address %q", s)
-	}
-
-	return ip
-}
+//func randIpAddressMust(t *testing.T, cidrBlock string) net.IP {
+//	s, err := acctest.RandIpAddress(cidrBlock)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	ip := net.ParseIP(s)
+//	if ip == nil {
+//		t.Fatalf("randIpAddressMust failed to parse IP address %q", s)
+//	}
+//
+//	return ip
+//}
