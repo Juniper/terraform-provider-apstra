e5bdaece89651d2832048eabdc422b0260fe7e7e
Chris Marget <cmarget@juniper.net>
2024-01-13 20:56:19 -0500

    Introduce `apstra_datacenter_security_policy` resource

--- apstra/blueprint/constants.go
+++ apstra/blueprint/constants.go
@@ -6,6 +6,7 @@ const (
 	errProviderBug           = "Provider Bug. Please report this issue to the provider maintainers."
 	errApiCompatibility      = "Apstra API version incompatibility"
 	errInvalidConfig         = "invalid configuration"
+	errStringParse           = "failed to parse string value"
 
 	ErrDCBlueprintCreate = "Failed to create client for Datacenter Blueprint %s"
 )
