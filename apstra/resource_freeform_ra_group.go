30c49a05edab1dc6247198da89177578f2d2522f
bwJuniper <98770420+bwJuniper@users.noreply.github.com>
2024-07-22 21:01:20 +0200

    fixed ra_group items, examples, make docs

---
+++ apstra/resource_freeform_ra_group.go
@@ -0,0 +1,211 @@
+package tfapstra
+
+import (
+	"context"
+	"fmt"
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	"github.com/Juniper/terraform-provider-apstra/apstra/blueprint"
+	"github.com/Juniper/terraform-provider-apstra/apstra/utils"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ resource.ResourceWithConfigure = &resourceFreeformRaGroup{}
+	_ resourceWithSetFfBpClientFunc  = &resourceFreeformRaGroup{}
+	_ resourceWithSetBpLockFunc      = &resourceFreeformRaGroup{}
+)
+
+type resourceFreeformRaGroup struct {
+	getBpClientFunc func(context.Context, string) (*apstra.FreeformClient, error)
+	lockFunc        func(context.Context, string) error
+}
+
+func (o *resourceFreeformRaGroup) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_freeform_ra_group"
+}
+
+func (o *resourceFreeformRaGroup) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	configureResource(ctx, o, req, resp)
+}
+
+func (o *resourceFreeformRaGroup) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: docCategoryFreeform + "This resource creates a Resource Allocation Group in a Freeform Blueprint.",
+		Attributes:          blueprint.FreeformRaGroup{}.ResourceAttributes(),
+	}
+}
+
+func (o *resourceFreeformRaGroup) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	// Retrieve values from plan
+	var plan blueprint.FreeformRaGroup
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// get a client for the Freeform reference design
+	bp, err := o.getBpClientFunc(ctx, plan.BlueprintId.ValueString())
+	if err != nil {
+		if utils.IsApstra404(err) {
+			resp.Diagnostics.AddError(fmt.Sprintf("blueprint %s not found", plan.BlueprintId), err.Error())
+			return
+		}
+		resp.Diagnostics.AddError("failed to create blueprint client", err.Error())
+		return
+	}
+
+	// Lock the blueprint mutex.
+	err = o.lockFunc(ctx, plan.BlueprintId.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			fmt.Sprintf("error locking blueprint %q mutex", plan.BlueprintId.ValueString()),
+			err.Error())
+		return
+	}
+
+	// Convert the plan into an API Request
+	request := plan.Request(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	id, err := bp.CreateRaGroup(ctx, request)
+	if err != nil {
+		resp.Diagnostics.AddError("error creating new Resource Allocation Group", err.Error())
+		return
+	}
+
+	plan.Id = types.StringValue(id.String())
+	plan.GeneratorId = types.StringNull()
+
+	// set state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+func (o *resourceFreeformRaGroup) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state blueprint.FreeformRaGroup
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// get a client for the Freeform reference design
+	bp, err := o.getBpClientFunc(ctx, state.BlueprintId.ValueString())
+	if err != nil {
+		if utils.IsApstra404(err) {
+			resp.Diagnostics.AddError(fmt.Sprintf("blueprint %s not found", state.BlueprintId), err.Error())
+			return
+		}
+		resp.Diagnostics.AddError("failed to create blueprint client", err.Error())
+		return
+	}
+
+	api, err := bp.GetRaGroup(ctx, apstra.ObjectId(state.Id.ValueString()))
+	if err != nil {
+		if utils.IsApstra404(err) {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+		resp.Diagnostics.AddError("Error retrieving Freeform Resource Allocation Group", err.Error())
+		return
+	}
+
+	state.LoadApiData(ctx, api.Data, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Set state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (o *resourceFreeformRaGroup) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	// Get plan values
+	var plan blueprint.FreeformRaGroup
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// get a client for the Freeform reference design
+	bp, err := o.getBpClientFunc(ctx, plan.BlueprintId.ValueString())
+	if err != nil {
+		if utils.IsApstra404(err) {
+			resp.Diagnostics.AddError(fmt.Sprintf("blueprint %s not found", plan.BlueprintId), err.Error())
+			return
+		}
+		resp.Diagnostics.AddError("failed to create blueprint client", err.Error())
+		return
+	}
+
+	// Lock the blueprint mutex.
+	err = o.lockFunc(ctx, plan.BlueprintId.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			fmt.Sprintf("error locking blueprint %q mutex", plan.BlueprintId.ValueString()),
+			err.Error())
+		return
+	}
+
+	request := plan.Request(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Update Config Template
+	err = bp.UpdateRaGroup(ctx, apstra.ObjectId(plan.Id.ValueString()), request)
+	if err != nil {
+		resp.Diagnostics.AddError("error updating Freeform Resource Allocation Group", err.Error())
+		return
+	}
+	plan.GeneratorId = types.StringNull()
+	// set state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+func (o *resourceFreeformRaGroup) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state blueprint.FreeformRaGroup
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// get a client for the Freeform reference design
+	bp, err := o.getBpClientFunc(ctx, state.BlueprintId.ValueString())
+	if err != nil {
+		if utils.IsApstra404(err) {
+			return // 404 is okay
+		}
+		resp.Diagnostics.AddError("failed to create blueprint client", err.Error())
+		return
+	}
+
+	// Lock the blueprint mutex.
+	err = o.lockFunc(ctx, state.BlueprintId.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			fmt.Sprintf("error locking blueprint %q mutex", state.BlueprintId.ValueString()),
+			err.Error())
+		return
+	}
+
+	// Delete Config Template by calling API
+	err = bp.DeleteRaGroup(ctx, apstra.ObjectId(state.Id.ValueString()))
+	if err != nil {
+		if utils.IsApstra404(err) {
+			return // 404 is okay
+		}
+		resp.Diagnostics.AddError("error deleting Freeform Resource Allocation Group", err.Error())
+		return
+	}
+}
+
+func (o *resourceFreeformRaGroup) setBpClientFunc(f func(context.Context, string) (*apstra.FreeformClient, error)) {
+	o.getBpClientFunc = f
+}
+
+func (o *resourceFreeformRaGroup) setBpLockFunc(f func(context.Context, string) error) {
+	o.lockFunc = f
+}
