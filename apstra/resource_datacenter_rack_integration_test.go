e7fd5c4b5c3f41ade23ab4bdd86b99a30caf5cbe
Chris Marget <cmarget@juniper.net>
2025-05-04 01:08:08 -0400

    gofumpt

--- apstra/resource_datacenter_rack_integration_test.go
+++ apstra/resource_datacenter_rack_integration_test.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/go-version"
 	"github.com/Juniper/apstra-go-sdk/apstra"
 	tfapstra "github.com/Juniper/terraform-provider-apstra/apstra"
 	testutils "github.com/Juniper/terraform-provider-apstra/apstra/test_utils"
+	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
