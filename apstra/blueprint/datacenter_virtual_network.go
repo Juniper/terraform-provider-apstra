6c2f82cb209cfb92028548b77494514fbec4bbdc
Chris Marget <cmarget@juniper.net>
2025-07-07 09:44:45 -0400

    update markdown string

--- apstra/blueprint/datacenter_virtual_network.go
+++ apstra/blueprint/datacenter_virtual_network.go
@@ -443,8 +443,9 @@ func (o DatacenterVirtualNetwork) ResourceAttributes() map[string]resourceSchema
 			},
 		},
 		"dhcp_service_enabled": resourceSchema.BoolAttribute{
-			MarkdownDescription: "Enables a DHCP relay agent. Note that enabling this feature when no `bindings` are " +
-				"configured may lead to state churn because a VN with no bindings cannot retain the *enabled* state.",
+			MarkdownDescription: "Enables a DHCP relay agent. Note that configuring this feature without configuring " +
+				"any `bindings` may lead to state churn because a VN with no bindings does not retain the " +
+				"`dhcp_service_enabled` state.",
 			Optional: true,
 			Computed: true,
 			Default:  booldefault.StaticBool(false),
