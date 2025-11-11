23a30c85937281f6acb5a947e5a81f00510ecd60
Chris Marget <cmarget@juniper.net>
2025-11-11 18:42:00 -0500

    go-licenses as tool

--- scripts/compliance.sh
+++ scripts/compliance.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-TPC=Third_Party_Code
+TPC="Third_Party_Code"
 
 # Default values
 minimal_mode=false
@@ -23,51 +23,37 @@ done
 IGNORE=()
 IGNORE+=(--ignore)
 IGNORE+=(github.com/Juniper) # don't bother with Juniper licenses
-IGNORE+=(--ignore)
-IGNORE+=(golang.org/x/sys) # explained below
 
-# golang.org/x/sys is ignored to avoid producing different results on different platforms (x/sys vs. x/sys/unix, etc...)
-# The license details for this package, if they were included in the Third_Party_Code directory, would look something
-# like this, depending on the build platform:
+# determine GOBIN and create it if necessary
+if [ "${GOBIN:-}" == "" ]; then
+  GOBIN="${GOPATH:-}/bin"
+fi
+if [ "${GOBIN:-}" == "" ]; then
+  GOBIN="${HOME:-}/go/bin"
+fi
+if [ "${GOBIN:-}" == "" ]; then
+  echo "failed to set GOBIN"
+  exit 1
+fi
+export GOBIN
+mkdir -p "$GOBIN"
 
-    ### golang.org/x/sys/unix
-    #
-    #* Name: golang.org/x/sys/unix
-    #* Version: v0.17.0
-    #* License: [BSD-3-Clause](https://cs.opensource.google/go/x/sys/+/v0.17.0:LICENSE)
-    #
-    #
-    #Copyright (c) 2009 The Go Authors. All rights reserved.
-    #
-    #Redistribution and use in source and binary forms, with or without
-    #modification, are permitted provided that the following conditions are
-    #met:
-    #
-    #   * Redistributions of source code must retain the above copyright
-    #notice, this list of conditions and the following disclaimer.
-    #   * Redistributions in binary form must reproduce the above
-    #copyright notice, this list of conditions and the following disclaimer
-    #in the documentation and/or other materials provided with the
-    #distribution.
-    #   * Neither the name of Google Inc. nor the names of its
-    #contributors may be used to endorse or promote products derived from
-    #this software without specific prior written permission.
-    #
-    #THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    #"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    #LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-    #A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-    #OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    #SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-    #LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-    #DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-    #THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    #(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-    #OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# determine the version string and binary-with-version file path
+VER=$(cd tools/go-licenses && go list -m github.com/google/go-licenses/v2 | awk '{print $2}')
+BIN="$GOBIN/go-licenses-$VER"
 
+# build the binary if we don't have it
+if [ ! -x "$BIN" ]; then
+  TMPDIR=$(mktemp -d)
+  (cd tools/go-licenses && GOBIN="$TMPDIR" go install tool github.com/google/go-licenses/v2)
+  mv "$TMPDIR/go-licenses" $BIN
+  rm -rf "$TMPDIR"
+fi
 
-go run github.com/google/go-licenses/v2 save   ${IGNORE[@]} --save_path "${TPC}" --force ./...
-go run github.com/google/go-licenses/v2 report ${IGNORE[@]} --template .notices.tpl ./... > "${TPC}/NOTICES.md"
+$BIN save   ${IGNORE[@]} --save_path "${TPC}" --force ./...
+$BIN report ${IGNORE[@]} --template .notices.tpl ./... > "${TPC}/NOTICES.md"
+$BIN report ${IGNORE[@]} --template <(printf "{{ range . }}{{ .LicenseName }}\n{{ end }}") ./... | sort -u > "${TPC}/all_licenses.txt"
+
 
 # The `save` command above collects only license and notice files from packages with licenses identified as
 # `RestrictionsShareLicense` and collects the entire source tree when the license is identified as
@@ -80,7 +66,7 @@ go run github.com/google/go-licenses/v2 report ${IGNORE[@]} --template .notices.
 if [[ "$minimal_mode" == true ]]; then
     echo "Removing third party source files."
     # The line below deletes "saved" files other than those beginning with "LICENSE" and "NOTICE"
-    find "$TPC" -type f ! -name 'LICENSE*' ! -name 'NOTICE*' -print0 | xargs -0 rm --
+    find "$TPC" -type f ! -name 'LICENSE*' ! -name 'NOTICE*' ! -name 'all_licenses.txt' -print0 | xargs -0 rm --
 
     # We now likely have some empty directories. Get rid of 'em.
     find "$TPC" -depth -type d -empty -exec rmdir -- "{}" \;
