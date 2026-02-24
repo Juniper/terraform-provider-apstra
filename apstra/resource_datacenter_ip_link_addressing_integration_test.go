6535097be7d44ae36074caa3790c1224d1469131
chrismarget-j <cmarget@juniper.net>
2026-02-24 18:48:08 -0500

    1201 testdatasourcedatacenterconnectivitytemplatesstatus fails with 610 (#1207)

--- apstra/resource_datacenter_ip_link_addressing_integration_test.go
+++ apstra/resource_datacenter_ip_link_addressing_integration_test.go
@@ -9,12 +9,10 @@ import (
 	"testing"
 
 	"github.com/Juniper/apstra-go-sdk/apstra"
-	"github.com/Juniper/apstra-go-sdk/enum"
 	tfapstra "github.com/Juniper/terraform-provider-apstra/apstra"
 	testutils "github.com/Juniper/terraform-provider-apstra/apstra/test_utils"
 	"github.com/Juniper/terraform-provider-apstra/internal/pointer"
 	"github.com/hashicorp/go-version"
-	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/stretchr/testify/require"
 )
@@ -126,13 +124,7 @@ func TestResourceDatacenterIpLinkAddressing(t *testing.T) {
 	rzCount := 2
 	rzIds := make([]string, rzCount)
 	for i := range rzIds {
-		name := acctest.RandString(6)
-		rzIds[i], err = bp.CreateSecurityZone(ctx, apstra.SecurityZone{
-			Label:   name,
-			VRFName: name,
-			Type:    enum.SecurityZoneTypeEVPN,
-		})
-		require.NoError(t, err)
+		rzIds[i] = testutils.SecurityZoneB(t, ctx, bp, false)
 	}
 
 	// discover IPv4 and IPv6 pools
