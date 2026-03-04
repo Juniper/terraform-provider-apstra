22972fbf76193d61e6ea4bba9e4e56f9bb67f50a
Chris Marget <cmarget@juniper.net>
2026-03-03 21:02:36 -0500

    add todo comment

--- apstra/resource_datacenter_routing_zone.go
+++ apstra/resource_datacenter_routing_zone.go
@@ -185,6 +185,7 @@ func (o *resourceDatacenterRoutingZone) Create(ctx context.Context, req resource
 	}
 
 	// make a security zone request
+	// todo: consider making vrf name optional - only overwrite with name when unknown.
 	plan.VrfName = plan.Name // copy whatever the user set as name in to VrfName
 	request := plan.Request(ctx, bp.Client(), &resp.Diagnostics)
 	if resp.Diagnostics.HasError() {
