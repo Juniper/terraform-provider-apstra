e71affbb030f69b50044a61138edc54c89ef72f3
Chris Marget <cmarget@juniper.net>
2026-09-16 19:20:54 -0400

    add system redundancy group membership cache

---
+++ internal/system_redundancy_info/lookup_integration_test.go
@@ -0,0 +1,164 @@
+package sysredundancyinfo_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	testutils "github.com/Juniper/terraform-provider-apstra/apstra/test_utils"
+	sysredundancyinfo "github.com/Juniper/terraform-provider-apstra/internal/system_redundancy_info"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLookup(t *testing.T) {
+	ctx := context.Background()
+
+	bp := testutils.BlueprintC(t, ctx)
+	bpID := string(bp.Id())
+	expectedGroupCount := 4
+	expectedSystemCount := 15
+
+	clearBPToSystemToGroupCache := func() {
+		for key := range sysredundancyinfo.BPToSystemToGroup {
+			delete(sysredundancyinfo.BPToSystemToGroup, key)
+		}
+	}
+	clearBPToGroupToSystemsCache := func() {
+		for key := range sysredundancyinfo.BPToGroupToSystem {
+			delete(sysredundancyinfo.BPToGroupToSystem, key)
+		}
+	}
+
+	t.Run("lookup_systems_using_bogus_group_id", func(t *testing.T) {
+		// Begin by clearing the cache.
+		clearBPToGroupToSystemsCache()
+		clearBPToSystemToGroupCache()
+
+		var diags diag.Diagnostics
+		systems := sysredundancyinfo.LookupSystem(ctx, bp, "bogus-group-id", &diags)
+		require.True(t, diags.HasError(), "expected error for bogus group ID, but got none")
+		require.Empty(t, systems[0], "expected first member of bogus redundant system pair to have empty ID")
+		require.Empty(t, systems[1], "expected second member of bogus redundant system pair to have empty ID")
+		require.Equal(t, 1, len(sysredundancyinfo.BPToGroupToSystem))                         // one blueprint in the group->system cache
+		require.Equal(t, expectedGroupCount, len(sysredundancyinfo.BPToGroupToSystem[bpID]))  // expectedGroupCount groups in the per-bp cache
+		require.Equal(t, 1, len(sysredundancyinfo.BPToSystemToGroup))                         // one blueprint in the system->group cache
+		require.Equal(t, expectedSystemCount, len(sysredundancyinfo.BPToSystemToGroup[bpID])) // expectedSystemCount systems in the per-bp cache
+	})
+
+	t.Run("lookup_group_using_bogus_system_id", func(t *testing.T) {
+		// Begin by clearing the cache.
+		clearBPToGroupToSystemsCache()
+		clearBPToSystemToGroupCache()
+
+		var diags diag.Diagnostics
+		group := sysredundancyinfo.LookupGroup(ctx, bp, "bogus-system-id", &diags)
+		require.Nilf(t, group, "expected nil group for bogus system id")
+		require.True(t, diags.HasError(), "expected error for bogus system ID, but got none")
+		require.Equal(t, 1, len(sysredundancyinfo.BPToGroupToSystem))                         // one blueprint in the group->system cache
+		require.Equal(t, expectedGroupCount, len(sysredundancyinfo.BPToGroupToSystem[bpID]))  // expectedGroupCount groups in the per-bp cache
+		require.Equal(t, 1, len(sysredundancyinfo.BPToSystemToGroup))                         // one blueprint in the system->group cache
+		require.Equal(t, expectedSystemCount, len(sysredundancyinfo.BPToSystemToGroup[bpID])) // expectedSystemCount systems in the per-bp cache
+	})
+
+	// Function which returns system IDs of switch nodes.
+	switchIDs := func(t testing.TB, ctx context.Context, bp *apstra.TwoStageL3ClosClient) []string {
+		q := new(apstra.PathQuery).
+			SetClient(bp.Client()).
+			SetBlueprintId(apstra.ObjectId(bpID)).
+			Node([]apstra.QEEAttribute{
+				apstra.NodeTypeSystem.QEEAttribute(),
+				{Key: "system_type", Value: apstra.QEStringVal(apstra.SystemTypeSwitch.String())},
+				{Key: "name", Value: apstra.QEStringVal("n_sys")},
+			})
+
+		var target struct {
+			Items []struct {
+				System struct {
+					ID string `json:"id"`
+				} `json:"n_sys"`
+			} `json:"items"`
+		}
+
+		require.NoError(t, q.Do(ctx, &target))
+		result := make([]string, len(target.Items))
+		for i, item := range target.Items {
+			result[i] = item.System.ID
+		}
+		return result
+	}
+
+	// Function which returns redundancy group node IDs.
+	groupIDs := func(t testing.TB, ctx context.Context, bp *apstra.TwoStageL3ClosClient) []string {
+		q := new(apstra.PathQuery).
+			SetClient(bp.Client()).
+			SetBlueprintId(apstra.ObjectId(bpID)).
+			Node([]apstra.QEEAttribute{
+				apstra.NodeTypeRedundancyGroup.QEEAttribute(),
+				{Key: "name", Value: apstra.QEStringVal("n_grp")},
+			})
+
+		var target struct {
+			Items []struct {
+				Group struct {
+					ID string `json:"id"`
+				} `json:"n_grp"`
+			} `json:"items"`
+		}
+
+		require.NoError(t, q.Do(ctx, &target))
+		result := make([]string, len(target.Items))
+		for i, item := range target.Items {
+			result[i] = item.Group.ID
+		}
+		return result
+	}
+
+	// Create maps to keep track of every system and group we encounter during the following tests.
+	systemIDSet := make(map[string]struct{})
+	groupIDSet := make(map[string]struct{})
+
+	t.Run("lookup_all_groups", func(t *testing.T) {
+		// Begin by clearing the cache.
+		clearBPToGroupToSystemsCache()
+		clearBPToSystemToGroupCache()
+
+		groupCount := 0
+		for _, groupID := range groupIDs(t, ctx, bp) {
+			groupCount++
+			groupIDSet[groupID] = struct{}{}
+			var diags diag.Diagnostics
+			systemIDs := sysredundancyinfo.LookupSystem(ctx, bp, groupID, &diags)
+			require.False(t, diags.HasError())
+			require.NotEmpty(t, systemIDs[0])
+			require.NotEmpty(t, systemIDs[1])
+			systemIDSet[systemIDs[0]] = struct{}{}
+			systemIDSet[systemIDs[1]] = struct{}{}
+		}
+		require.Equal(t, expectedGroupCount, groupCount)
+	})
+
+	t.Run("lookup_all_systems", func(t *testing.T) {
+		// Begin by clearing the cache.
+		clearBPToGroupToSystemsCache()
+		clearBPToSystemToGroupCache()
+
+		redundantSystemCount := 0
+		for _, systemID := range switchIDs(t, ctx, bp) {
+			systemIDSet[systemID] = struct{}{}
+			var diags diag.Diagnostics
+			groupID := sysredundancyinfo.LookupGroup(ctx, bp, systemID, &diags)
+			require.False(t, diags.HasError())
+			if groupID != nil {
+				groupIDSet[*groupID] = struct{}{}
+				redundantSystemCount++
+			}
+		}
+		require.Equal(t, expectedGroupCount*2, redundantSystemCount)
+	})
+
+	require.Equal(t, expectedGroupCount, len(groupIDSet))
+	require.Equal(t, expectedSystemCount, len(systemIDSet))
+	require.Equal(t, expectedGroupCount, len(sysredundancyinfo.BPToGroupToSystem[bpID]))
+	require.Equal(t, expectedSystemCount, len(sysredundancyinfo.BPToSystemToGroup[bpID]))
+}
